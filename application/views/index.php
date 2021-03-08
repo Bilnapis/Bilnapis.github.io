11625bed9cc6e3571b127eff365c8c236eb02ff3
abil <nafisabil08@gmail.com>
2021-03-08 17:32:40 +0700

    ui: Add animation text

--- application/views/index.php
+++ application/views/index.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="https://anumainterior.herokuapp.com/assets/css/styles.css" rel="stylesheet" />
+    <link href="<?= base_url('assets/') ?>css/styles.css" rel="stylesheet" />
     <!-- <link rel="stylesheet" href="<?= base_url('assets/') ?>font-awesome/css/font-awesome.min.css"> -->
 
     <style>
@@ -83,6 +83,41 @@
             opacity: 0;
             transition: opacity ease-in-out 0.25s;
         }
+
+        /* Animation Text */
+        /* 
+        .ml11 {
+            font-weight: 700;
+            font-size: 3.5em;
+        } */
+
+        .ml11 .text-wrapper {
+            position: relative;
+            display: inline-block;
+            padding-top: 0.1em;
+            padding-right: 0.05em;
+            padding-bottom: 0.15em;
+        }
+
+        .ml11 .line {
+            opacity: 0;
+            position: absolute;
+            left: 0;
+            height: 100%;
+            width: 3px;
+            background-color: #fff;
+            transform-origin: 0 50%;
+        }
+
+        .ml11 .line1 {
+            top: 0;
+            left: 0;
+        }
+
+        .ml11 .letter {
+            display: inline-block;
+            line-height: 1em;
+        }
     </style>
 </head>
 
@@ -107,10 +142,42 @@
     </nav>
 
     <!-- Masthead-->
+    <!-- <div class="container">
+        <div class="box">
+
+            <div class="title">
+                <span class="block"></span>
+                <h1>Kaio Almeida<span></span></h1>
+            </div>
+
+            <div class="role">
+                <div class="block"></div>
+                <p>UI Dev designer</p>
+            </div>
+
+        </div>
+    </div> -->
     <header class="masthead">
         <div class="container">
-            <div class="masthead-subheading">Welcome To Our Shop!</div>
-            <div class="masthead-heading text-uppercase">Your Room, Your Taste</div>
+            <!-- <h1 class="ml11">
+                <span class="text-wrapper">
+                    <span class="line line1"></span>
+                    <span class="letters">Hello Goodbye</span>
+                </span>
+            </h1> -->
+
+            <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script> -->
+            <!-- <h1 class="ml3">Great Thinkers</h1> -->
+            <!-- <div class="masthead-subheading">Welcome To Our Shop!</div> -->
+            <!-- <h1 class="ml11">
+                <span class="text-wrapper">
+                    <span class="line line1"></span>
+                    <span class="masthead-subheading letters">Welcome to Our Shop !</span>
+                </span>
+            </h1>
+        </div> -->
+            <div class="masthead-subheading">Welcome to Our Shop !</div>
+            <div class="masthead-heading text-uppercase ml3">Your Room, Your Taste</div>
             <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
         </div>
     </header>
@@ -550,6 +617,88 @@
     <script src="<?= base_url('assets/'); ?>assets/mail/contact_me.js"></script>
     <!-- Core theme JS-->
     <script src="<?= base_url('assets/'); ?>js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script>
+        // Wrap every letter in a span
+        var textWrapper = document.querySelector('.ml3');
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+        anime.timeline({
+                loop: false
+            })
+            .add({
+                targets: '.ml3 .letter',
+                opacity: [0, 1],
+                easing: "easeInOutQuad",
+                duration: 100,
+                delay: (el, i) => 150 * (i + 1)
+            })
+        // .add({
+        //     targets: '.ml3',
+        //     opacity: 0,
+        //     duration: 1000,
+        //     easing: "easeOutExpo",
+        //     delay: 1000
+        // });
+
+        var textWrapper = document.querySelector('.ml7');
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+        anime.timeline({
+                loop: false
+            })
+            .add({
+                targets: '.ml7 .letter',
+                opacity: [0, 1],
+                easing: "easeInOutQuad",
+                duration: 2250,
+                delay: (el, i) => 150 * (i + 1)
+            })
+
+        // Wrap every letter in a span
+        var textWrapper = document.querySelector('.ml11 .letters');
+        textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+        anime.timeline({
+                loop: false
+            })
+            // .add({
+            //     targets: '.ml11 .letter',
+            //     opacity: [0, 1],
+            //     easing: "easeInOutQuad",
+            //     duration: 100,
+            //     delay: (el, i) => 150 * (i + 1)
+            // })
+            .add({
+                targets: '.ml11 .line',
+                scaleY: [0, 1],
+                opacity: [0.5, 1],
+                easing: "easeOutExpo",
+                duration: 700
+            })
+            .add({
+                targets: '.ml11 .line',
+                translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+                easing: "easeOutExpo",
+                duration: 700,
+                delay: 100
+            })
+            .add({
+                targets: '.ml11 .letter',
+                opacity: [0, 1],
+                easing: "easeOutExpo",
+                duration: 600,
+                offset: '-=775',
+                delay: (el, i) => 34 * (i + 1)
+            });
+        // .add({
+        //     targets: '.ml11',
+        //     opacity: 0,
+        //     duration: 1000,
+        //     easing: "easeOutExpo",
+        //     delay: 1000
+        // });
+    </script>
 </body>
 
 </html>
