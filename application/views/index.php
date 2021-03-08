bdc86ec8b99550e1a2fa1b233e11db5db8ebc43e
abil <nafisabil08@gmail.com>
2021-03-08 17:52:44 +0700

    merapikan code

--- application/views/index.php
+++ application/views/index.php
@@ -142,40 +142,8 @@
     </nav>
 
     <!-- Masthead-->
-    <!-- <div class="container">
-        <div class="box">
-
-            <div class="title">
-                <span class="block"></span>
-                <h1>Kaio Almeida<span></span></h1>
-            </div>
-
-            <div class="role">
-                <div class="block"></div>
-                <p>UI Dev designer</p>
-            </div>
-
-        </div>
-    </div> -->
     <header class="masthead">
         <div class="container">
-            <!-- <h1 class="ml11">
-                <span class="text-wrapper">
-                    <span class="line line1"></span>
-                    <span class="letters">Hello Goodbye</span>
-                </span>
-            </h1> -->
-
-            <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script> -->
-            <!-- <h1 class="ml3">Great Thinkers</h1> -->
-            <!-- <div class="masthead-subheading">Welcome To Our Shop!</div> -->
-            <!-- <h1 class="ml11">
-                <span class="text-wrapper">
-                    <span class="line line1"></span>
-                    <span class="masthead-subheading letters">Welcome to Our Shop !</span>
-                </span>
-            </h1>
-        </div> -->
             <div class="masthead-subheading">Welcome to Our Shop !</div>
             <div class="masthead-heading text-uppercase ml3">Your Room, Your Taste</div>
             <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
@@ -632,72 +600,7 @@
                 easing: "easeInOutQuad",
                 duration: 100,
                 delay: (el, i) => 150 * (i + 1)
-            })
-        // .add({
-        //     targets: '.ml3',
-        //     opacity: 0,
-        //     duration: 1000,
-        //     easing: "easeOutExpo",
-        //     delay: 1000
-        // });
-
-        var textWrapper = document.querySelector('.ml7');
-        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-        anime.timeline({
-                loop: false
-            })
-            .add({
-                targets: '.ml7 .letter',
-                opacity: [0, 1],
-                easing: "easeInOutQuad",
-                duration: 2250,
-                delay: (el, i) => 150 * (i + 1)
-            })
-
-        // Wrap every letter in a span
-        var textWrapper = document.querySelector('.ml11 .letters');
-        textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
-
-        anime.timeline({
-                loop: false
-            })
-            // .add({
-            //     targets: '.ml11 .letter',
-            //     opacity: [0, 1],
-            //     easing: "easeInOutQuad",
-            //     duration: 100,
-            //     delay: (el, i) => 150 * (i + 1)
-            // })
-            .add({
-                targets: '.ml11 .line',
-                scaleY: [0, 1],
-                opacity: [0.5, 1],
-                easing: "easeOutExpo",
-                duration: 700
-            })
-            .add({
-                targets: '.ml11 .line',
-                translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
-                easing: "easeOutExpo",
-                duration: 700,
-                delay: 100
-            })
-            .add({
-                targets: '.ml11 .letter',
-                opacity: [0, 1],
-                easing: "easeOutExpo",
-                duration: 600,
-                offset: '-=775',
-                delay: (el, i) => 34 * (i + 1)
             });
-        // .add({
-        //     targets: '.ml11',
-        //     opacity: 0,
-        //     duration: 1000,
-        //     easing: "easeOutExpo",
-        //     delay: 1000
-        // });
     </script>
 </body>
 
